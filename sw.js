f0e404fbc582b7cfc5571b7e8f18f390bb550de6
John Erik Halse <johnerikhalse@gmail.com>
2016-09-26 20:21:35 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,6 +3,4 @@ console.log("SW startup");
 this.onfetch = function(event) {
   console.log("Fetch event", event);
   console.log(".request", event.request);
-  console.log(".respondWith", event.respondWith);
-  console.log(".default", event.default);
 };
